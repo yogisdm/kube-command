75ba7fc9ed44f2fd94120a9821a49e5b6fd14f7c
unknown <yogisdmec007@gmail.com>
2023-04-21 12:21:56 +0530

    code update

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,7 +12,7 @@ pipeline {
  
     stage('terraform version') {
       steps {
-	    sh 'terraform init -reconfigure'
+	    sh 'terraform init'
         sh 'terraform --version'
 		sh 'terraform plan'
       }
--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 terraform {
   backend "s3" {
     bucket = "yogi-tf"
     key    = "terraform-backend/eks-test.tf"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
 
